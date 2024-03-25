b9af4793d46124a4db993f632de7a9ec702fd263
Andrii Medintsev <medintsev.andrew@gmail.com>
2024-03-25 15:02:56 +0200

    fonts optimization

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
 import '../styles/reset.css';
 import '../styles/global.css';
 
+import { inter } from '@/app/fonts';
 import { StoreContextProvider } from '@/context/store';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <StoreContextProvider>{children}</StoreContextProvider>
       </body>
     </html>
